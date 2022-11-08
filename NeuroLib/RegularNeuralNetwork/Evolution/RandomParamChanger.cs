d0e312e6bc0cf702957b6c5bac6192134127fb8a
Evhen Diulin <ydiulin@rework-space.com>
2022-11-08 12:16:54 +0200

    RandomParamChanger modifier

---
+++ NeuroLib/RegularNeuralNetwork/Evolution/RandomParamChanger.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace NeuroLib.RegularNeuralNetwork.Evolution
+{
+	public class RandomParamChanger : Modifier<NeuralNetwork>
+	{
+		private Random _rnd;
+
+
+		public RandomParamChanger(Random rnd)
+		{
+			_rnd = rnd;
+		}
+
+
+		public override NeuralNetwork Modify(NeuralNetwork original)
+		{
+			if (_rnd.Next() % 2 == 0)
+			{
+				_ChangeRandomBias(original);
+			}
+			else
+			{
+				_ChangeRandomWeight(original);
+			}
+
+			return original;
+		}
+
+
+		private void _ChangeRandomBias(NeuralNetwork net)
+		{
+			int layer = _rnd.Next(net.AmountOfLayers - 1) + 1;
+
+			float[] biases = net.GetBiasVector(layer);
+			int x = _rnd.Next(biases.Length);
+
+			biases[x] = _ChangeValueRandomly(biases[x]);
+		}
+
+
+		private void _ChangeRandomWeight(NeuralNetwork net)
+		{
+			int layer = _rnd.Next(net.AmountOfLayers - 1) + 1;
+
+			float[,] weights = net.GetWeightMatrix(layer);
+			int x = _rnd.Next(weights.GetLength(0));
+			int y = _rnd.Next(weights.GetLength(1));
+
+			weights[x, y] = _ChangeValueRandomly(weights[x, y]);
+		}
+
+
+		private float _ChangeValueRandomly(float originalValue)
+		{
+			int precision = _rnd.Next(7);
+			int order = originalValue == 0.0f
+			? 0
+				: (int)Math.Log10(Math.Abs(originalValue));
+
+			float scale = (float)Math.Pow(10, order - precision);
+			float difference = (float)_rnd.NextDouble() * 2 - 1;
+			return originalValue + scale * difference;
+		}
+	}
+}
---
+++ NeuroLibTest/RandomParamChangerTest.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeuroLib;
+using NeuroLib.RegularNeuralNetwork.Evolution;
+using System;
+
+namespace NeuroLibTest
+{
+	[TestClass]
+	public class RandomParamChangerTest
+	{
+
+		/// <summary>
+		/// All params (weights and biases) should be changed during the test.
+		/// If one of them is eqal to zero, most likely you did something wrong.
+		/// </summary>
+		[TestMethod]
+		public void ItShouldChangeAllParamsInTheNetwork()
+		{
+			NeuralNetwork net = new NeuralNetwork(3, 3, 3);
+			RandomParamChanger changer = new RandomParamChanger(new Random());
+
+			for (int i = 0; i < 10000; i++)
+			{
+				changer.Modify(net);
+			}
+
+			_TestNetworkParams(net);
+		}
+
+
+		private void _TestNetworkParams(NeuralNetwork net)
+		{
+			int[] prms = net.GetConstructorParams();
+			for (int i = 1; i < prms.Length; i++)
+			{
+				_TestLayerParams(net, i);
+			}
+		}
+
+
+		private void _TestLayerParams(NeuralNetwork net, int layer)
+		{
+			float[] biases = net.GetBiasVector(layer);
+			for (int i = 0; i < biases.Length; i++)
+			{
+				Assert.AreNotEqual(0, biases[i]);
+			}
+
+
+			float[,] weights = net.GetWeightMatrix(layer);
+			for (int i = 0; i < weights.GetLength(0); i++)
+			{
+				for (int j = 0; j < weights.GetLength(1); j++)
+				{
+					Assert.AreNotEqual(0, weights[i, j]);
+				}
+			}
+		}
+	}
+}
