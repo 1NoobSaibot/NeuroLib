f93851929d5dc00b2a8e666654dc3251f271c857
Evhen Diulin <zheka.diulin@gmail.com>
2022-12-29 17:44:28 +0200

    Added ModificationQueue

---
+++ NeuroLib/ModificationQueue.cs
@@ -0,0 +1,24 @@
+﻿namespace NeuroLib
+{
+	public class ModificationQueue<T> : IModifier<T>
+	{
+		private IModifier<T>[] _modifiers;
+
+
+		public ModificationQueue(params IModifier<T>[] modifiers)
+		{
+			_modifiers = modifiers;
+		}
+
+
+		public T Modify(T original)
+		{
+			T res = original;
+			foreach (var modifier in _modifiers)
+			{
+				res = modifier.Modify(res);
+			}
+			return res;
+		}
+	}
+}
