812bec8b4f553298383e9bb81ee36cdafa38b144
Evhen Diulin <zheka.diulin@gmail.com>
2025-02-12 16:53:49 +0200

    Add class NeuralNetworkCUDA to make code a little prettier

---
+++ NeuroLib/RegularNeuralNetwork/NeuralNetworkCUDA.cs
@@ -0,0 +1,135 @@
+﻿using NeuroLib.Layers;
+using NeuroLib.Math;
+
+
+namespace NeuroLib
+{
+	public class NeuralNetworkCUDA
+	{
+		private readonly ILayer[] _layers;
+		public int AmountOfLayers => _layers.Length;
+		private readonly InputLayer _inputLayer;
+		private readonly DenseLayerTrainable _outputLayer;
+		public readonly int BatchSize;
+
+
+		public NeuralNetworkCUDA(int batchSize, int[] amountsOfNeurons)
+		{
+			if (batchSize < 1)
+			{
+				throw new ArgumentException($"must be a positive number", nameof(batchSize));
+			}
+			if (amountsOfNeurons.Length < 2)
+			{
+				throw new ArgumentException("Neural network cannot have less than two layers of neurons");
+			}
+
+			BatchSize = batchSize;
+			_layers = new ILayer[amountsOfNeurons.Length];
+			_inputLayer = new InputLayer(amountsOfNeurons[0], batchSize);
+			_layers[0] = _inputLayer;
+			for (int i = 1; i < _layers.Length; i++)
+			{
+				_layers[i] = new DenseLayerTrainable(_layers[i - 1], amountsOfNeurons[i]);
+			}
+
+			_outputLayer = (DenseLayerTrainable)_layers[^1];
+		}
+
+
+		public void Forward(float[] input, float[] output)
+		{
+			if (input.Length != _inputLayer.Inputs.ElementCount)
+			{
+				throw new ArgumentException("Length must be equal to product of inputLength * batchSize", nameof(input));
+			}
+			if (output.Length != _outputLayer.Outputs.ElementCount)
+			{
+				throw new ArgumentException("Length must be equal to product of outputLength * batchSize", nameof(output));
+			}
+
+			_inputLayer.Inputs.SetValues(input);
+			_inputLayer.Forward();
+			_outputLayer.Outputs.GetValues(output);
+		}
+
+
+		public void Randomize(Random rnd)
+		{
+			for (int i = 1; i < _layers.Length; i++)
+			{
+				DenseLayerTrainable layer = (DenseLayerTrainable)_layers[i];
+				layer.RandomiseWeights(rnd);
+			}
+		}
+
+
+		public void Fit(Random rnd, float[][] inputDataSet, float[][] trueOutValues, int epochs)
+		{
+			if (inputDataSet.Length != trueOutValues.Length)
+			{
+				throw new ArgumentException("Lengthes of data sets must be equal to each other");
+			}
+			if (inputDataSet.Length < BatchSize)
+			{
+				throw new ArgumentException("input data sets");
+			}
+			int inputLength = _inputLayer.Inputs.Columns;
+			int outputLength = _outputLayer.Outputs.Columns;
+			foreach (var inputVector in inputDataSet)
+			{
+				if (inputVector.Length != inputLength)
+				{
+					throw new ArgumentException($"One or more items has invalid length", nameof(inputDataSet));
+				}
+			}
+			foreach (var outputVector in trueOutValues)
+			{
+				if (outputVector.Length != outputLength)
+				{
+					throw new ArgumentException($"One or more items has invalid length", nameof(trueOutValues));
+				}
+			}
+
+			int[] dataSetOrder = CreateDataSetOrderArray(inputDataSet.Length);
+			float[] inputBatchBuffer = new float[_inputLayer.Inputs.ElementCount];
+			float[] outputBatchBuffer = new float[_outputLayer.Outputs.ElementCount];
+
+			CudaDeviceMatrix trueOut = CudaDeviceMatrix.CreateWithShapeOf(_outputLayer.Outputs);
+			for (int epoch = 0; epoch < epochs; epoch++)
+			{
+				rnd.Shuffle(dataSetOrder);
+				for (int batchIndex = 0; batchIndex < inputDataSet.Length; batchIndex += BatchSize)
+				{
+					for (int itemIndex = 0; itemIndex < BatchSize; itemIndex++)
+					{
+						int index = dataSetOrder[batchIndex + itemIndex];
+						float[] inputVector = inputDataSet[index];
+						float[] trueOutputVector = trueOutValues[index];
+
+						Array.Copy(inputVector, 0, inputBatchBuffer, itemIndex * inputLength, inputVector.Length);
+						Array.Copy(trueOutputVector, 0, outputBatchBuffer, itemIndex * outputLength, trueOutputVector.Length);
+					}
+
+					_inputLayer.Inputs.SetValues(inputBatchBuffer);
+					_inputLayer.Forward();
+					trueOut.SetValues(outputBatchBuffer);
+					_outputLayer.StartBackward(trueOut);
+				}
+			}
+
+
+			static int[] CreateDataSetOrderArray(int length)
+			{
+				int[] order = new int[length];
+				int index = 0;
+				while (index < length)
+				{
+					order[index] = index;
+					++index;
+				}
+				return order;
+			}
+		}
+	}
+}
--- NeuroLibTest/Networks/Perceptron/SimplePerceptronTest.cs
+++ NeuroLibTest/Networks/Perceptron/SimplePerceptronTest.cs
@@ -1,5 +1,4 @@
-﻿using NeuroLib.Layers;
-using NeuroLib.Math;
+﻿using NeuroLib;
 
 namespace NeuroLibTest.Networks.Perceptron
 {
@@ -13,15 +12,9 @@ namespace NeuroLibTest.Networks.Perceptron
 			const int BATCH_SIZE = 1;
 			const int seed = 184131478;
 
-			Random rnd = new Random(seed);
-			InputLayer input = new(2, BATCH_SIZE);
-			DenseLayerTrainable hiddenLayer = new(input, 2);
-			hiddenLayer.RandomiseWeights(rnd);
-			DenseLayerTrainable output = new(hiddenLayer, 1);
-			output.RandomiseWeights(rnd);
-
-			TrainableLayerTracker hiddenTracker = new(hiddenLayer);
-			TrainableLayerTracker outputTracker = new(output);
+			Random rnd = new(seed);
+			NeuralNetworkCUDA nn = new(BATCH_SIZE, [2, 2, 1]);
+			nn.Randomize(rnd);
 
 			float[][] inputDataSets = [
 				[ 0, 0 ],
@@ -31,104 +24,22 @@ namespace NeuroLibTest.Networks.Perceptron
 			];
 
 			float[][] trueOutValues = [[0], [1], [1], [0]];
+			nn.Fit(rnd, inputDataSets, trueOutValues, epochs: 1000);
 
-			float error = Fit();
-
-			Assert.IsTrue(error <= TARGET_MAX_ERROR, $"The error={error} is bigger than expected: {TARGET_MAX_ERROR}");
-			Console.WriteLine("The error is just " + error);
-
-			float Fit()
+			float maxAbsError = 0;
+			float[] outputBuffer = new float[1];
+			for (int i = 0; i < inputDataSets.Length; i++)
 			{
-				int[] dataSetOrder = [0, 1, 2, 3];
-				CudaDeviceMatrix trueOut = new(rows: 1, columns: 1);
-				float[] actualVectorBuffer = new float[1];
-				float lastError = float.MaxValue;
-				for (int epoch = 0; epoch < 1000; epoch++)
+				nn.Forward(inputDataSets[i], outputBuffer);
+				float localError = Math.Abs(outputBuffer[0] - trueOutValues[i][0]);
+				if (localError > maxAbsError)
 				{
-					rnd.Shuffle(dataSetOrder);
-					float theBiggestErrorInTheEpoch = 0;
-					foreach (var i in dataSetOrder)
-					{
-						float[] inputVector = inputDataSets[i];
-						float[] trueOutputVector = trueOutValues[i];
-
-						input.Inputs.SetValues(inputVector);
-						input.Forward();
-						output.Outputs.GetValues(actualVectorBuffer);
-						float error = GetMaxAbsoluteError(trueOutputVector, actualVectorBuffer);
-						if (error > theBiggestErrorInTheEpoch)
-						{
-							theBiggestErrorInTheEpoch = error;
-						}
-
-						trueOut.SetValues(trueOutputVector);
-						output.StartBackward(trueOut);
-					}
-					lastError = theBiggestErrorInTheEpoch;
-				}
-
-				return lastError;
-			}
-
-			void TrackDeviceData()
-			{
-				hiddenTracker.Update();
-				outputTracker.Update();
-			}
-		}
-
-
-		private static float GetMaxAbsoluteError(float[] expected, float[] actual)
-		{
-			Assert.AreEqual(expected.Length, actual.Length);
-
-			float maxAbsoluteError = 0;
-			for (int i = 0; i < expected.Length; i++)
-			{
-				float localError = Math.Abs(expected[i] - actual[i]);
-				if (localError > maxAbsoluteError)
-				{
-					maxAbsoluteError = localError;
+					maxAbsError = localError;
 				}
 			}
 
-			return maxAbsoluteError;
-		}
-
-
-		private class TrainableLayerTracker
-		{
-			public float[] Inputs;
-			public float[] Weights;
-			public float[] ZSum;
-			public float[] Outputs;
-			public float[] InputDeltas;
-			public float[] OutputDeltas;
-			private readonly DenseLayerTrainable _m;
-
-
-			public TrainableLayerTracker(DenseLayerTrainable layer)
-			{
-				_m = layer;
-
-				Inputs = new float[layer.Inputs.ElementCount];
-				Weights = new float[layer.WeightsAndBiases.ElementCount];
-				ZSum = new float[layer.Sum_Z.ElementCount];
-				Outputs = new float[layer.Outputs.ElementCount];
-				InputDeltas = new float[layer.InputDeltas.ElementCount];
-				OutputDeltas = new float[layer.OutputDeltas.ElementCount];
-			}
-
-
-			public void Update()
-			{
-				_m.Inputs.GetValues(Inputs);
-				_m.WeightsAndBiases.GetValues(Weights);
-				_m.Sum_Z.GetValues(ZSum);
-				_m.Outputs.GetValues(Outputs);
-				_m.InputDeltas.GetValues(InputDeltas);
-				_m.OutputDeltas.GetValues(OutputDeltas);
-			}
+			Assert.IsTrue(maxAbsError <= TARGET_MAX_ERROR, $"The error={maxAbsError} is bigger than expected: {TARGET_MAX_ERROR}");
+			Console.WriteLine("The error is just " + maxAbsError);
 		}
 	}
 }
